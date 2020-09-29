31d780b0f8e34a5ae1d438397704ec0788dff0e1
MaicolBen <maicol.bentancor@gmail.com>
2020-09-29 17:30:17 -0300

    Add search to Ticket object

--- lib/freshdesk/ticket.rb
+++ lib/freshdesk/ticket.rb
@@ -1,7 +1,7 @@
 module Freshdesk
   class Ticket < FreshdeskObject
     class << self
-      def create_a_ticket(params: {})
+      def create(params: {})
         new("/tickets").resource(params: params).post
       end
 
@@ -9,40 +9,44 @@ module Freshdesk
         new("/tickets/outbound_email").resource(params: params).post
       end
 
-      def view_a_ticket(id:, query: "")
+      def retrieve(id:, query: "")
         new("/tickets/#{id}#{query}").resource(id: id).get
       end
 
-      def list_all_tickets(query: "")
+      def list(query: "")
         new("/tickets#{query}").resource.get
       end
 
-      def update_a_ticket(id:, params: {})
+      def update(id:, params: {})
         new("/tickets/#{id}").resource(id: id, params: params).put
       end
 
-      def delete_a_ticket(id:)
+      def delete(id:)
         new("/tickets/#{id}").resource(id: id).delete
       end
 
-      def restore_a_ticket(id:)
+      def restore(id:)
         new("/tickets/#{id}/restore").resource(id: id).put
       end
 
-      def list_all_ticket_fields
-        new("/ticket_fields").resource.get
+      def list_all_fields
+        new('/ticket_fields').resource.get
       end
 
-      def list_all_conversations_of_a_ticket(id:)
+      def list_all_conversations(id:)
         new("/tickets/#{id}/conversations").resource(id: id).get
       end
 
-      def list_all_time_entries_of_a_ticket(id:)
+      def list_all_time_entries(id:)
         new("/tickets/#{id}/time_entries").resource(id: id).get
       end
 
-      def list_all_satisfaction_ratings_of_a_ticket(ticket_id:)
+      def list_all_satisfaction_ratings(ticket_id:)
         new("/tickets/#{ticket_id}/satisfaction_ratings").resource.get
+      end
+
+      def search(query: "")
+        new("/search/tickets?query=#{query}").resource.get
       end
     end
   end
